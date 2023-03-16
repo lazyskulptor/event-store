3387ce2c93274e0e58fd5cc90d947f8734bcd4c3
Park Hyeonjun <lazyskulptor@gmail.com>
2023-03-16 23:19:17 +0900

    Enhance Test and Debugging

--- src/lazyskulptor/es/spec.clj
+++ src/lazyskulptor/es/spec.clj
@@ -3,14 +3,17 @@
     [clojure.spec.gen.alpha :as gen]
     [clojure.spec.alpha :as s]))
 
-(s/def :lazyskulptor.es.core/uuid-string
+(s/def :lazyskulptor.es/uuid-string
   (s/with-gen #(uuid? (java.util.UUID/fromString %))
               #(gen/fmap (fn [_] (.toString (random-uuid))) (gen/string))))
-(s/def :lazyskulptor.es.core/entity-id :lazyskulptor.es.core/uuid-string)
-(s/def :lazyskulptor.es./event-type string?)
-(s/def :lazyskulptor.es.core/value (s/or :map map? :nil nil?))
+(s/def :lazyskulptor.es/entity-id :lazyskulptor.es/uuid-string)
+(s/def :lazyskulptor.es/event-type (s/or :string string? :keyword keyword?))
+(s/def :lazyskulptor.es/value (s/or :map map? :nil nil?))
+(s/def :lazyskulptor.es/time (s/or :nil nil?
+                   :date #(= (type %) java.util.Date)
+                   :instant #(= (type %) java.time.Instant)))
 
-(s/def :lazyskulptor.es.core/event
-  (s/keys :req-un [:lazyskulptor.es.core/entity-id
-                   :lazyskulptor.es.core/event-type
-                   :lazyskulptor.es.core/value]))
+(s/def :lazyskulptor.es/event
+  (s/keys :req-un [:lazyskulptor.es/entity-id
+                   :lazyskulptor.es/event-type
+                   :lazyskulptor.es/value]))
